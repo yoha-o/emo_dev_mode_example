2e0186d0d46debe82c5e1091a4df6acc8d5a58e2
yoha-o <ooishi408@gmail.com>
2024-10-18 05:20:27 +0900

    add,impl: FastAPI, gitignore

--- emo_dev_mode_example/main.py
+++ emo_dev_mode_example/main.py
@@ -1,99 +1,85 @@
 import os
 from dotenv import load_dotenv
-import json, http.server
+from fastapi import FastAPI, Request
 from pyngrok import ngrok
 from emo_platform import Client, Tokens, WebHook, EmoPlatformError
+
 
 load_dotenv()
 PLATFORM_API_URL = os.getenv('PLATFORM_API_URL')
 PLATFORM_API_REFRESH_TOKEN = os.getenv('PLATFORM_API_REFRESH_TOKEN')
 HTTP_SERVER_PORT = os.getenv('HTTP_SERVER_PORT')
 
+app = FastAPI()
 client = Client(
-    endpoint_url = PLATFORM_API_URL,
-    tokens=Tokens(refresh_token = PLATFORM_API_REFRESH_TOKEN)
-    )
-
-public_url = ngrok.connect(int(HTTP_SERVER_PORT)).public_url
-client.create_webhook_setting(WebHook(public_url))
+	endpoint_url = PLATFORM_API_URL,
+	tokens=Tokens(refresh_token = PLATFORM_API_REFRESH_TOKEN)
+	)
 
 
 @client.event('trigger_word.detected')
 def trigger_word_callback(data):
-    print(data)
+	print(data)
 
 @client.event('vui_command.detected')
 def vui_command_callback(data):
-    print(data)
+	print(data)
 
 @client.event('record_button.pressed')
 def record_button_callback(data):
-    print(data)
+	print(data)
 
 @client.event('play_button.pressed')
 def play_button_callback(data):
-    print(data)
+	print(data)
 
 @client.event('function_button.pressed')
 def function_button_callback(data):
-    print(data)
+	print(data)
 
 @client.event('accel.detected')
 def accel_sensor_callback(data):
-    print(data)
+	print(data)
 
 @client.event('illuminance.changed')
 def illuminance_sensor_callback(data):
-    print(data)
+	print(data)
 
 @client.event('radar.detected')
 def radar_sensor_callback(data):
-    print(data)
+	print(data)
 
 @client.event('function_button.pressed')
 def function_button_callback(data):
-    print(data)
+	print(data)
 
 
 secret_key = client.start_webhook_event()
 
-class Handler(http.server.BaseHTTPRequestHandler):
-	def _send_status(self, status):
-		self.send_response(status)
-		self.send_header('Content-type', 'text/plain; charset=utf-8')
-		self.end_headers()
-		
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'text/plain')
-		self.end_headers()
-		self.wfile.write(b'Hello, World!')
-		
-	def do_POST(self):
-		if not secret_key == self.headers["X-Platform-Api-Secret"]:
-			self._send_status(401)
-			return
-		
-		content_len = int(self.headers['content-length'])
-		request_body = json.loads(self.rfile.read(content_len).decode('utf-8'))
+@app.get('/health')
+async def do_submit():
+	return {'health': 'OK'}
 
+@app.post('/')
+async def receive_webhook(request: Request):
+	if secret_key == request.headers['X-Platform-Api-Secret']:
+		body = await request.json()
+		
 		try:
-			cb_func, emo_webhook_body = client.get_cb_func(request_body)
+			cb_func, emo_webhook_body = client.get_cb_func(body)
 		except EmoPlatformError:
-			self._send_status(501)
-			return
+			return {'message': 'emo platform error'}
 		
 		cb_func(emo_webhook_body)
-		
-		self._send_status(200)
-
-with http.server.HTTPServer(('', int(HTTP_SERVER_PORT)), Handler) as httpd:
-	httpd.serve_forever()
+		return {'message': 'success'}
 
 
 def main():
-    print("Hello, world!")
+	import uvicorn
+	public_url = ngrok.connect(int(HTTP_SERVER_PORT)).public_url
+	client.create_webhook_setting(WebHook(public_url))
+	uvicorn.run(app, host='0.0.0.0', port=int(HTTP_SERVER_PORT))
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+	main()
